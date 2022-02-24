d6955799c2807244041639650876baa1caa23e75
Sofia <sofia.montgomery@gmail.com>
2022-02-24 22:38:14 +0100

    edit index.js

--- public/js/index.js
+++ public/js/index.js
@@ -18,19 +18,18 @@ function createTable(data){
     var appElement = document.getElementById("highscore")
     var aHeading = document.createElement("h3")
     aHeading.innerHTML = "HIGHSCORE"
-    var aPicture = document.createElement("image")
-    aPicture.innerHTML = "https://openclipart.org/image/400px/173367"
     appElement.appendChild(aHeading)
     var aTable = document.createElement("table")
     appElement.appendChild(aTable)
-    aTable.appendChild(createRow(data[0].name, data[0].points, data[0].player))
-    aTable.appendChild(createRow(data[1].name, data[1].points, data[1].player))
-    aTable.appendChild(createRow(data[2].name, data[2].points, data[2].player))
-    aTable.appendChild(createRow(data[3].name, data[3].points, data[3].player))
+    aTable.appendChild(createRow(data[0].name, data[0].points, data[0].player, data[0].url))
+    aTable.appendChild(createRow(data[1].name, data[1].points, data[1].player, data[1].url))
+    aTable.appendChild(createRow(data[2].name, data[2].points, data[2].player, data[2].url))
+    aTable.appendChild(createRow(data[3].name, data[3].points, data[3].player, data[3].url))
 
 }
-function createRow(name, points, player){
+function createRow(name, points, player, url){
     var aRow = document.createElement("tr")
+    aRow.appendChild(createImageCell(url))
     aRow.appendChild(createCell(name))
     aRow.appendChild(createCell(points))
     aRow.appendChild(createCell(player))
@@ -42,5 +41,13 @@ function createCell(content){
     return aCell;
 
 
+}
+function createImageCell(url){
+    var aCell = document.createElement("td")
+    var anImage = document.createElement("img")
+    anImage.src=url
+    anImage.classList.add("tableLogo")
+    aCell.appendChild(anImage)
+    return aCell;
 }
 
