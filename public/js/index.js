3a530ce2377f6f1324b2c8997549ff9fccce02f0
Sofia <sofia.montgomery@gmail.com>
2022-02-25 13:05:31 +0100

    edit design

--- public/js/index.js
+++ public/js/index.js
@@ -10,28 +10,44 @@ function age(){
     xhr.onload = function() {
         var data = JSON.parse(this.response);
         createTable(data);
+    
     }
     xhr.send();
 }
 
 function createTable(data){
     var appElement = document.getElementById("highscore")
-    var aHeading = document.createElement("h3")
-    aHeading.innerHTML = "HIGHSCORE"
+
+    var mainHeading = document.createElement("h3")
+    mainHeading.innerHTML = "Fastest player"
+    appElement.appendChild(mainHeading)
+
+    /* var aHeading = document.createElement("h4")
+    aHeading.innerHTML = "Level 1"
     appElement.appendChild(aHeading)
+
     var aTable = document.createElement("table")
     appElement.appendChild(aTable)
-    aTable.appendChild(createRow(data[0].name, data[0].points, data[0].player, data[0].image))
-    aTable.appendChild(createRow(data[1].name, data[1].points, data[1].player, data[1].image))
-    aTable.appendChild(createRow(data[2].name, data[2].points, data[2].player, data[2].image))
-    aTable.appendChild(createRow(data[3].name, data[3].points, data[3].player, data[3].image))
+    aTable.appendChild(createRow(data[0].time, data[0].player, data[0].image))
+    aTable.appendChild(createRow(data[1].time, data[1].player, data[1].image))
+    aTable.appendChild(createRow(data[2].time, data[2].player, data[2].image))
+    
+
+    var anotherHeading = document.createElement("h4")
+    anotherHeading.innerHTML = "Level 2"
+    appElement.appendChild(anotherHeading)
+    var anotherTable = document.createElement("table")
+    appElement.appendChild(anotherTable)
+    anotherTable.appendChild(createRow(data[3].time, data[3].player, data[3].image))
+    anotherTable.appendChild(createRow(data[1].time, data[1].player, data[1].image))
+    anotherTable.appendChild(createRow(data[2].time, data[2].player, data[2].image)) */
+
 
 }
-function createRow(name, points, player, url){
+function createRow(time, player, url){
     var aRow = document.createElement("tr")
     aRow.appendChild(createImageCell(url))
-    aRow.appendChild(createCell(name))
-    aRow.appendChild(createCell(points))
+    aRow.appendChild(createCell(time))
     aRow.appendChild(createCell(player))
     return aRow;
 }
--- public/js/levelOne.js
+++ public/js/levelOne.js
@@ -4,41 +4,28 @@ age();
 
 function age(){
     var xhr = new XMLHttpRequest()
-    xhr.open("GET", "C:\Java\kursenJavaScript\sti-frontend\public\js\levelOne.json")
+    xhr.open("GET", "/js/data.json")
     xhr.onload = function() {
         var data = JSON.parse(this.response);
-        createCardStack(data)
+        createCard(data);
     }
     xhr.send()
 }
 
-function createCardStack(){
+function createCard(data){
     var appElement = document.getElementById("levelOne")
-    var aCard = document.createElement("image")
+    var aCard = document.createElement("img")
     appElement.appendChild(aCard)
-
-
-    aTable.appendChild(createCard(data[0].name, data[0].image))
-    aTable.appendChild(createCard(data[1].image))
-    aTable.appendChild(createCard(data[2].image))
-    aTable.appendChild(createCard(data[3].image))
-    aTable.appendChild(createCard(data[4].image))
-    aTable.appendChild(createCard(data[5].image))
-    aTable.appendChild(createCard(data[6].image))
-    aTable.appendChild(createCard(data[7].image))
-    aTable.appendChild(createCard(data[8].image))
-    aTable.appendChild(createCard(data[9].image))
-    aTable.appendChild(createCard(data[10].image))
-    aTable.appendChild(createCard(data[11].image))
-    aTable.appendChild(createCard(data[12].image))
-    aTable.appendChild(createCard(data[13].image))
-    aTable.appendChild(createCard(data[14].image))
-    aTable.appendChild(createCard(data[15].image))
+    aCard.appendChild(createImage(data[0].image))
 
 }
 
-function createCard(image){
-    var aCard = document.createElement("image")
-    aRow.appendChild(createCell(image))
-    return aCard;
+function createImage(url){
+    var anImage = document.createElement("img")
+    anImage.src=url
+    anImage.classList.add("image")
+    aCell.appendChild(anImage)
+    return anImage;
 }
+
+const section = document.querySelector('section');
